6f8ee21ff48f7a9c20b25a9f817899516b91b75a
zhxysky <zhxysky@qq.com>
2014-08-14 15:53:57 +0800

    add log

--- src/main/java/com/zhxy/controller/IndexAction.java
+++ src/main/java/com/zhxy/controller/IndexAction.java
@@ -1,15 +1,19 @@
 package com.zhxy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexAction {
 
+	Logger logger = LoggerFactory.getLogger(IndexAction.class);
 	
 	@RequestMapping("/hello.do")
 	public String hello() {
 		System.out.println("hello......");
+		logger.info("hello info.");
 		return "hello";
 	}
 }
