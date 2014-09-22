3857af3210606285cc86a6e6ed75ba91b5917216
zhxysky <zhxysky@qq.com>
2014-09-22 15:37:43 +0800

    use restful url

--- src/main/java/com/zhxy/controller/IndexAction.java
+++ src/main/java/com/zhxy/controller/IndexAction.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zhxy.bean.User;
 import com.zhxy.service.UserService;
@@ -20,15 +22,15 @@ public class IndexAction {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping("/hello.do")
-	public String hello() {
-		System.out.println("hello......");
+	@RequestMapping("/hello/{name}")
+	public @ResponseBody String hello(@PathVariable String name) {
+		System.out.println("hello "+name);
 		logger.info("hello info.");
-		return "hello";
+		return "hello "+name;
 	}
 	
 	
-	@RequestMapping("/userList.do")
+	@RequestMapping("/userList")
 	public String userList(ModelMap model) {
 		List<User> userList = userService.getUserList();
 		userService.getUserById(1);
