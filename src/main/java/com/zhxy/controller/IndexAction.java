93df2a33d23cc782e1c4845ab64ab76d42516f29
zhxysky <zhxysky@qq.com>
2014-10-30 16:21:07 +0800

    print br1

--- src/main/java/com/zhxy/controller/IndexAction.java
+++ src/main/java/com/zhxy/controller/IndexAction.java
@@ -26,6 +26,7 @@ public class IndexAction {
 	public @ResponseBody String hello(@PathVariable String name) {
 		System.out.println("hello "+name);
 		logger.info("hello info.");
+		System.out.println("br1..");
 		return "hello "+name;
 	}
 	
