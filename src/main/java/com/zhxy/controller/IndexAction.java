92a14e5c3d3b91829c26354a3068dfac3635071c
zhxysky <zhxysky@qq.com>
2014-10-31 14:11:25 +0800

    print merge1

--- src/main/java/com/zhxy/controller/IndexAction.java
+++ src/main/java/com/zhxy/controller/IndexAction.java
@@ -31,6 +31,7 @@ public class IndexAction {
 		System.out.println("br");
 		System.out.println("rebase 1.");
 		System.out.println("rebase 2");
+		System.out.println("merge 1");
 		return "hello "+name;
 	}
 	
